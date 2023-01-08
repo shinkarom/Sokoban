09372ec34fee264431f1871da78982c7cf260cba
shinkarom <shinkarom@gmail.com>
2023-01-08 17:38:36 +0200

    Add Level1

--- Scripts/Game.gd
+++ Scripts/Game.gd
@@ -7,12 +7,16 @@ var levelNumber = 0
 func setLevel(param):
 	if param >= 0 and param < totalLevels:
 		get_tree().change_scene("res://Levels/Level"+str(param)+".tscn")
+		levelNumber = param
 
 func nextLevel():
 	setLevel((levelNumber+1)%totalLevels)
 	
 func previousLevel():
-	setLevel((levelNumber-1)%totalLevels)
+	var prevNumber = levelNumber - 1
+	if prevNumber < 0:
+		prevNumber = totalLevels - 1 
+	setLevel(prevNumber)
 
 func reloadLevel():
 	setLevel(levelNumber)
